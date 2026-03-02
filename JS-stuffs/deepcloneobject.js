5d3e9cd7066187cb8827da57560ef81859aa3f70
Rohit Kumar <viciousdotro@gmail.com>
2026-03-02 18:03:39 +0530

    Add deepcloneobject.js
    Needed in React / Redux style updates

---
+++ JS-stuffs/deepcloneobject.js
@@ -0,0 +1,17 @@
+export function deepClone(obj) {
+  if (obj === null || typeof obj !== "object") {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map(item => deepClone(item));
+  }
+
+  const cloned = {};
+
+  for (let key in obj) {
+    cloned[key] = deepClone(obj[key]);
+  }
+
+  return cloned;
+}
