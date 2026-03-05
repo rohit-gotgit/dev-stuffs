14d388e44f0f680b65ca2a2670519f7267812bed
Rohit Kumar <viciousdotro@gmail.com>
2026-03-05 16:58:24 +0530

    Add limittxtlen.js
    If text exceeds maxLength, cut it and add ....

---
+++ JS-stuffs/limittxtlen.js
@@ -0,0 +1,9 @@
+export function truncateText(text, maxLength) {
+  if (typeof text !== "string") return "";
+
+  if (text.length <= maxLength) {
+    return text;
+  }
+
+  return text.slice(0, maxLength) + "...";
+}
