06db0691210b42d6a30a2bfac5634c96200f0454
Rohit Kumar <viciousdotro@gmail.com>
2026-03-09 14:39:31 +0530

    Add ScrolltoElement.js
    Scroll to Element

---
+++ JS-stuffs/ScrolltoElement.js
@@ -0,0 +1,9 @@
+export function scrollToElement(selector) {
+  const element = document.querySelector(selector);
+
+  if (element) {
+    element.scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+}
