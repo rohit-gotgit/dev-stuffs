ccdf5c37a3860b6cf46e867fa8a691c33c74c465
Rohit Kumar <viciousdotro@gmail.com>
2026-03-30 15:39:14 +0530

    Add errorboundary.jsx

---
+++ react-stuffs/errorboundary.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) return <h2>Error occurred</h2>;
+    return this.props.children;
+  }
+}
