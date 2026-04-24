acc4af23e5e4b173323e497e6cde5b5201cb7e49
Rohit Kumar <viciousdotro@gmail.com>
2026-04-24 09:50:36 +0530

    Add mousepostracker.jsx

---
+++ react-stuffs/mousepostracker.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export default function MouseTracker() {
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const move = (e) => setPos({ x: e.clientX, y: e.clientY });
+
+    window.addEventListener("mousemove", move);
+    return () => window.removeEventListener("mousemove", move);
+  }, []);
+
+  return (
+    <p>
+      X: {pos.x}, Y: {pos.y}
+    </p>
+  );
+}
